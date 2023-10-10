312f4f549a170d925cd3adc563c4202c8c4e6290
Devin <devinveneyofficial@gmail.com>
2023-10-10 10:08:24 +0900

    Hero section edit

--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -7,12 +7,25 @@ const Hero = () => (
   <div className="hero" id="home">
     <div className="hero-overlay">
       <div className="description">
-        <h2 className="title">hELLO. i aM PRANTOSH</h2>
-        <h3 className="tagline">Breaking keyboards and pushing pixels since 2018</h3>
-        <p className="paragraph">I'm passionate about crafting exceptional digital experiences. I bring ideas to life in the virtual realm with a keyboard as my paintbrush and lines of code as my canvas. Let's join forces and bring your digital dreams to fruition. Get in touch, and let's embark on this exciting journey together!</p>
+        <h2 className="title">welcome! </h2>
+        <h3 className="tagline">
+          Breaking keyboards and pushing pixels since 2016
+        </h3>
+        <p className="paragraph">
+          I am driven by a deep passion for creating remarkable digital
+          journeys. With my keyboard as a tool and lines of code as my medium, I
+          have the ability to transform your digital visions into reality. Let's
+          collaborate and turn your digital dreams into tangible experiences.
+          Don't hesitate to reach out, and together, we can embark on this
+          thrilling adventure!
+        </p>
         <div className="hero-btns">
           <button className="hire-me">
-            <a href="https://drive.google.com/file/d/1RIQlxcFspH89xvugEeEyvgSG_ABMX3rI/view?usp=drive_link" target="_blank" rel="noreferrer">
+            <a
+              href="https://drive.google.com/file/d/1RIQlxcFspH89xvugEeEyvgSG_ABMX3rI/view?usp=drive_link"
+              target="_blank"
+              rel="noreferrer"
+            >
               GET MY CV
             </a>
           </button>
@@ -23,25 +36,20 @@ const Hero = () => (
           </button>
         </div>
       </div>
-      <img src={Imu} alt="prantosh" className="hero-image" />
+      <img src={Imu} alt="DevinVeney" className="hero-image" />
     </div>
     <div className="social-icons">
-        <a href="https://github.com/PrantoshB" target="_blank" rel="noreferrer">
-          <i className="fa-brands fa-github" />
-        </a>
-        <a href="https://www.linkedin.com/in/prantosh" target="_blank" rel="noreferrer">
-          <i className="fa-brands fa-linkedin-in" />
-        </a>
-        <a href="https://wellfound.com/u/prantosh-biswas" target="_blank" rel="noreferrer">
-          <i className="fab fa-angellist" />
-        </a>
-        <a href="https://www.twitter.com/prantalks" target="_blank" rel="noreferrer">
-          <i className="fa-brands fa-twitter" />
-        </a>
-         <a href="https://www.youtube.com/@prantalks" target="_blank" rel="noreferrer">
-          <i className="fa-brands fa-youtube" />
-        </a>
-      </div>
+      <a href="https://github.com/DevinVeney" target="_blank" rel="noreferrer">
+        <i className="fa-brands fa-github" />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/devin-veney-0ab460226/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i className="fa-brands fa-linkedin-in" />
+      </a>
+    </div>
   </div>
 );
 
